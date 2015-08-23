9ed2b44ba0fea245a4f0879fdc6aff37278a15ec
Luiza Nunes <luhhsnunes@gmail.com>
2015-08-23 10:55:44 -0500

    Refatoração do teste para criação de um voo.

--- tests/flights_test.js
+++ tests/flights_test.js
@@ -8,16 +8,7 @@ var contentType = "Content-Type:application/json";
 describe("HTTP Requests on Flight API", function() {
     it("should create a new flight", function () {
         var response = chakram.post(URL,
-          {
-            "data_partida": "01-10-2015",
-            "data_chegada": "10-10-2015",
-            "numero": "2222",
-            "origem": "CNF",
-            "destino": "UIO",
-            "duracao": 6,
-            "lugares": 10,
-            "companhia": "Avianca"
-          },
+          require('./data.json'),
           contentType
           );
         expect(response).to.have.status(200);
